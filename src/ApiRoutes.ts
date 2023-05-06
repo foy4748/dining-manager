6bf19c3c16150e9a18d98c3a7500c4f95771a086
foy4748 <faisaljfcl@gmail.com>
2023-05-06 21:08:34 +0600

    Getting Activation Request data. Gotta put some styles

--- src/ApiRoutes.ts
+++ src/ApiRoutes.ts
@@ -2,6 +2,7 @@ const SERVER_ADDRESS = import.meta.env.VITE_SERVER_ADDRESS;
 
 const ApiRoutes = {
   get: {
+    activate_meal: `${SERVER_ADDRESS}/activate-meal`,
     deactivate_meal: `${SERVER_ADDRESS}/deactivate-meal/`,
   },
   post: {
--- src/Components/ActivationRequest.tsx
+++ src/Components/ActivationRequest.tsx
@@ -7,12 +7,34 @@ import ApiRoutes from "../ApiRoutes";
 import DatePickerField from "../Forms/DatePickerField";
 import getTommorowDate from "../Utilities/getTommorowDate";
 import toast from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
 
 export default function ActivationRequest() {
   // [IMPORTANT]
   // Load Committe Data during login
   // to access Committe number
   const [disableSubmit, setDisableSubmit] = useState<Boolean>(false);
+
+  const { data: activationRequestData, isLoading } = useQuery({
+    queryKey: ["activation-request-data"],
+    queryFn: async () => {
+      try {
+        const options = {
+          headers: {
+            card_no: "242",
+            User_id: "123456789123456789123456",
+          },
+        };
+        const res = await fetch(ApiRoutes.get.activate_meal, options);
+        const data = await res.json();
+        console.log(data);
+        setDisableSubmit(false);
+        return data || {};
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  });
 
   const handleSubmit = async (submittingData: unknown) => {
     setDisableSubmit(true);
@@ -40,21 +62,32 @@ export default function ActivationRequest() {
     setDisableSubmit(false);
   };
 
+  if (isLoading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h1>Send Meal Activation Request</h1>
       <br />
-      <Formik
-        initialValues={{ activation_date: getTommorowDate() }}
-        onSubmit={handleSubmit}
-      >
-        <Form>
-          <DatePickerField name="activation_date" oneTap={true} />
-          <Button disabled={disableSubmit} type="submit">
-            Submit
-          </Button>
-        </Form>
-      </Formik>
+      <div>
+        <Formik
+          initialValues={{ activation_date: getTommorowDate() }}
+          onSubmit={handleSubmit}
+        >
+          <Form>
+            <DatePickerField name="activation_date" oneTap={true} />
+            <Button disabled={disableSubmit} type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Formik>
+      </div>
+      <p>{JSON.stringify(activationRequestData)}</p>
     </div>
   );
 }
